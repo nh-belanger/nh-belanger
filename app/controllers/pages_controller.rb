4cef4e0fcc2479035e2e1cbab78e1b62f53f97e8
nh-belanger <nick.h.belanger@gmail.com>
2017-04-08 17:23:23 -0400

    adding resume download

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
   end
+
+  def download_resume
+    send_file "public/docs/NickBelangerResume.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'download_resume', to: "pages#download_resume"
+
+
   get "/pages/:page" => "pages#show"
   root 'pages#show', page: 'home'
 end
